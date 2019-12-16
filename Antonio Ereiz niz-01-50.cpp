39d0d6f905d690c1c18eb5b416e29b3bc9662292
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2019-12-16 09:10:35 +0100

    Add files via upload

---
+++ Antonio Ereiz niz-01-50.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	for(int i=0;i<5;i++){
+		for(int n=0;n<=9;n++){
+			cout<<i<<"-"<<n<<endl;
+			
+		}
+		
+	}
+	cout<<"5-0";
+	
+	system("Pause");
+	return 0;
+}
